d813b994666631786959b864cb30efeb0c615436
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-01-15 20:40:38 +0100

    Added Spotify developer parameters

--- auth.py
+++ auth.py
@@ -1,10 +1,11 @@
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+import client
 
 
 def login():
-    client_id = "your_client_id"
-    client_secret = "your_client_secret"
+    client_id = client.clientID
+    client_secret = client.clientSecret
     redirect_uri = "http://localhost:5000/callback"
     scope="user-read-playback-state user-modify-playback-state"
 
