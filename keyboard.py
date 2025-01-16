7f11383e8be7f3d130572a6f9c9950d332305794
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-01-16 20:03:31 +0100

    Added pyautogui to send characters

--- keyboard.py
+++ keyboard.py
@@ -1,49 +1,48 @@
 from gpiozero import RotaryEncoder, Button
 from signal import pause
-import gui
-import main
+import pyautogui
+import time
 
-#Buchstaben für die Tastatur
-characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", ".", ",", "!", "?", "@", "_", " "]
-selected_character = 0
+import config
 
-button = Button(27)                                                 #Vorläufiger GPIO Pin für den Button
-rotary = RotaryEncoder(a=17, b= 18, max_steps=len(characters))      #Vorläufiger GPIO Pin für den Rotary Encoder
 
-print(len(characters))
+print(len(config.characters))
 
 def buttonpress(self):
-    if main.display==0:
-        #select character
-        print("placeholder")                                         #Problem mit der Auswahl des Buchstabens im Webbrowser
-        #selected_character.update_letters()
+    if config.display==0:
+        send_char_to_spotify(config.characters[config.selected_character])
+        print("placeholder")      
+                                           #Problem mit der Auswahl des Buchstabens im Webbrowser
     else:
         #pause playback 
         print("placeholder")
         
-    print(characters[selected_character])
+    print(config.characters[config.selected_character])
 
 def change_character_plus():
     selected_character += 1
-    if selected_character > len(characters):
-        selected_character = len(characters)  
+    if selected_character > len(config.characters):
+        selected_character = len(config.characters)  
     if selected_character < 0:
         selected_character = 0
     
-    print(characters[selected_character])
+    print(config.characters[config.selected_character])
 
 def change_character_minus():
     selected_character -= 1
-    if selected_character >= len(characters):
-        selected_character = len(characters)  
+    if selected_character >= len(config.characters):
+        selected_character = len(config.characters)  
     if selected_character < 0:
         selected_character = 0
 
-    print(characters[selected_character])
+    print(config.characters[config.selected_character])
 
-button.when_pressed = buttonpress
-rotary.when_rotated = change_character_plus         #Weg finden um Rotierungsrichtung einzubinden
+config.button.when_pressed = buttonpress
+config.rotary.when_rotated(clockwise=change_character_plus(), counterclockwise=change_character_minus())
 
-
+def send_char_to_spotify(selected_char):
+    # Warte kurz, damit die Spotify-Seite richtig geladen ist
+    time.sleep(2)
+    pyautogui.write(config.characters[config.selected_character])
 
 pause()
