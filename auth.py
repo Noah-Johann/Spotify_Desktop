0a6790b124ccd229bcd067e7489ffa8abe990497
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-01-16 17:42:46 +0100

    Repaired auth

--- auth.py
+++ auth.py
@@ -6,7 +6,11 @@ import client
 def login():
     client_id = client.clientID
     client_secret = client.clientSecret
-    redirect_uri = "http://localhost:5000/callback"
+    redirect_uri = "http://localhost:8888/callback"
     scope="user-read-playback-state user-modify-playback-state"
 
     sp = Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="user-read-playback-state user-modify-playback-state"))
+    #print(sp.get_me())
+    print(sp.current_user())
+    print(sp.current_playback())
+    print("Login")
