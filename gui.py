86e1ccfcd2d1ddbad3f521e9039c584875dd24b8
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-01-16 17:42:15 +0100

    Build window opening

--- gui.py
+++ gui.py
@@ -1,1 +1,29 @@
 from PIL import Image, ImageDraw, ImageFont
+import customtkinter as ctk 
+from tkinter import *
+from PIL import Image, ImageTk
+import colorsys
+from PIL import Image
+import numpy as np
+import requests
+from typing import Optional
+import logging
+import main
+
+root=ctk.CTk()
+
+#Window Setup
+root.geometry("800x480")
+root.title("Spotify Controller")
+
+
+
+def startWindow():
+    root.mainloop()
+    print("window")
+
+def login_screen():
+    main.display=0
+
+def playback_screen():
+    main.display=1
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+'''  © 2025 Noah Johann  '''
+
 import gui
 import auth
 
 display=0           #0=Login; 1=Playback; 
 
-
-
-
+__name__ = "__main__"
 
 
 
@@ -13,3 +13,5 @@ if __name__ == "__main__":
     #app = SpotifyController()
     #app.mainloop()
     print("placeholder")
+    auth.login()
+    gui.startWindow()
