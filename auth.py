fe843b0f6ef11daea3f881a22953f24966eb2a2b
Noah <179856882+Noah-Johann@users.noreply.github.com>
2025-01-17 15:45:18 +0100

    Build webview window for Spotify login

--- auth.py
+++ auth.py
@@ -1,16 +1,37 @@
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 import client
+import webview
 
 
 def login():
-    client_id = client.clientID
-    client_secret = client.clientSecret
-    redirect_uri = "http://localhost:8888/callback"
-    scope="user-read-playback-state user-modify-playback-state"
+    print(login)
+    auth_manager = SpotifyOAuth(
+        client_id="client.clientID",
+        client_secret="client.clientSecret",
+        redirect_uri="http://localhost:8888/callback",
+        scope="user-read-playback-state user-modify-playback-state",
+    )
 
-    sp = Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="user-read-playback-state user-modify-playback-state"))
+    auth_url = auth_manager.get_authorize_url()
+    
+    def on_loaded(window):
+        current_url = window.get_current_url()
+        if "callback?code=" in current_url:
+            auth_code = current_url.split("code=")[-1].split("&")[0]
+            token_info = auth_manager.get_access_token(auth_code)
+            print("Access Token:", token_info["access_token"])
+
+            sp = Spotify(auth_manager=auth_manager)
+            current_user = sp.me()
+            print("Angemeldeter Benutzer:", current_user["display_name"])
+            
+            webview.destroy_window()
+
+    webview.create_window("Spotify Login", auth_url, 800, 480, on_top=True, frameless=True)     #fullscreen=True, for Raspberry Pi
+    webview.start()
+    
     #print(sp.get_me())
-    print(sp.current_user())
-    print(sp.current_playback())
+    #print(sp.current_user())
+    #print(sp.current_playback())
     print("Login")
--- gui.py
+++ gui.py
@@ -8,22 +8,38 @@ import numpy as np
 import requests
 from typing import Optional
 import logging
-import main
+import threading
 
-root=ctk.CTk()
-
-#Window Setup
-root.geometry("800x480")
-root.title("Spotify Controller")
+import config
+import auth
 
 
 
-def startWindow():
-    root.mainloop()
-    print("window")
+def create_window():
+    # First handle authentication
+    if config.display == 0:
+        print("Starting authentication...")
+        auth.login()  # Run auth first in main thread
+        
+    # Then create GUI
+    root = ctk.CTk()
+    root.geometry("800x480")
+    root.title("Spotify Controller")
+    ctk.set_appearance_mode("#000000")
+
+    if config.display == 0:
+        login_screen()
+    if config.display == 1:
+        playback_screen()
+
+    root.mainloop()  # Run GUI mainloop in main thread
 
 def login_screen():
-    main.display=0
+    #root.configure(bg="#ffffff")
+    print("loginscreen")
 
 def playback_screen():
-    main.display=1
+    config.display = 1
+
+
+
